947c172ad6b3cb8ff1770b6a5aa5d5933eaef449
Bhupathi-Raju <bhupathi.raju@zemosolabs.com>
2017-06-28 10:56:39 +0530

    Update data.java

--- Raju/assignment/data/data.java
+++ Raju/assignment/data/data.java
@@ -11,10 +11,15 @@ public class data {
         System.out.println(a);
         System.out.println(b);
     }
-    public void print2()
+    /*public void print2() 
     {
         int c,d;
         System.out.println(c);
         System.out.println(d);
-    }
+    }*/
 }
+
+/*Explanation : 
+In the above code, the blocked part of the code gives compile time error becuase the variables c & d were not initialised. Default
+initialisation in Java takes place only in fields that are in a class but not that are in a method. In the above code, a & b gets 
+initialised but not c & d.*/
